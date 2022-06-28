cc028bbb89d734a0153a60dbe6a9ac6a247bbc20
wuuconix <1521900139@qq.com>
2022-06-28 22:10:05 +0800

    done

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import fetch from "node-fetch"
+
+const url1 = "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt"
+const url2 = "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt"
+const url3 = "https://cdn.staticaly.com/gh/XIU2/TrackersListCollection/master/all.txt"
+
+const p1 = fetch(url1).then(res => res.text())
+const p2 = fetch(url2).then(res => res.text())
+const p3 = fetch(url3).then(res => res.text())
+
+Promise.all([p1, p2, p3]).then(res => {
+    console.log(res)
+})
