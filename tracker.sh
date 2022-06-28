f8cc34fc629698afb5e786474b72d2f2c807a514
wuuconix <1521900139@qq.com>
2022-06-28 23:01:25 +0800

    add proxy

--- tracker.sh
+++ tracker.sh
@@ -1,3 +1,5 @@
+export http_proxy="http://127.0.0.1:7890"
+export https_proxy=$http_proxy
 a=`curl -s https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt`
 b=`curl -s https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt`
 c=`curl -s https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt`
